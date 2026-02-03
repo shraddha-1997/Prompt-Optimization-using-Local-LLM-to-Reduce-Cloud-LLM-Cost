775ad1620a5adef512e24d82c5ded3d49a337084
Shraddha Gaikwad <114001098+shraddha-1997@users.noreply.github.com>
2026-02-03 19:11:51 +0530

    Add stage 2 and stage 3 graphs for optimization

--- graph.py
+++ graph.py
@@ -1,11 +1,23 @@
+
+
 from langgraph.graph import StateGraph
 from optimizer.optimizer_node import optimize_prompt
+#from groq.groq_node import call_groq
+from groq_node_pkg.groq_node import call_groq
 
-# Define graph state schema
-graph_builder = StateGraph(dict)
+# STAGE 2 GRAPH (optimizer only)
 
-graph_builder.add_node("optimizer", optimize_prompt)
-graph_builder.set_entry_point("optimizer")
-graph_builder.set_finish_point("optimizer")
+stage2_builder = StateGraph(dict)
+stage2_builder.add_node("optimizer", optimize_prompt)
+stage2_builder.set_entry_point("optimizer")
+stage2_builder.set_finish_point("optimizer")
+stage2_graph = stage2_builder.compile()
 
-graph = graph_builder.compile()
+
+# STAGE 3 GRAPH (Groq only)
+
+stage3_builder = StateGraph(dict)
+stage3_builder.add_node("groq", call_groq)
+stage3_builder.set_entry_point("groq")
+stage3_builder.set_finish_point("groq")
+stage3_graph = stage3_builder.compile()
